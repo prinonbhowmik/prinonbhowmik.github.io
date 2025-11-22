8ee33ee277d0f4f18406a9b89f6d5ffa423c10d8
prinonbhowmik <prinonarnob@gmail.com>
2025-11-23 01:15:34 +0600

    image fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3247884157"
+    serviceWorkerVersion: "4039269991"
   }
 });
